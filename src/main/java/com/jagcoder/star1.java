1247270a06fb24728b147d5d3722fafdcbdb630a
raghavendru <raghavendruppar@gmail.com>
2025-04-03 11:02:12 +0530

    star1 done

---
+++ src/main/java/com/jagcoder/star1.java
@@ -0,0 +1,42 @@
+package com.jagcoder;
+
+import java.util.Scanner;
+
+public class star1 {
+    public void pattern(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+        for (int i=n-1;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        star1 s = new star1();
+        s.pattern(n);
+    }
+}
