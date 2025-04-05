6218abe91bf52f0fba3cd14ebfa0a351d3798e6a
raghavendru <raghavendruppar@gmail.com>
2025-04-05 21:25:04 +0530

    anagram practice done

---
+++ src/main/java/com/jagcoder/Anagram.java
@@ -0,0 +1,34 @@
+package com.jagcoder;
+
+public class Anagram {
+    public boolean Ana(String str,String target) {
+        if (str.length() != target.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'a']++;
+            count[target.charAt(i) - 'a']--;
+        }
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Anagram a = new Anagram();
+        String str = "listen";
+        String target = "silent";
+        boolean result = a.Ana(str, target);
+        if (result) {
+            System.out.println(str + " and " + target + " are anagrams.");
+        } else {
+            System.out.println(str + " and " + target + " are not anagrams.");
+        }
+
+    }
+}
