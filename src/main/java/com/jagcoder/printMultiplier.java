43543ff2f63274182816e3ef387c886a4a77c72d
raghavendru <raghavendruppar@gmail.com>
2025-04-15 19:28:28 +0530

    printMultiplier done

---
+++ src/main/java/com/jagcoder/printMultiplier.java
@@ -0,0 +1,23 @@
+package com.jagcoder;
+
+import java.util.Arrays;
+
+public class printMultiplier {
+    public static int[] mul(int n) {
+        int[] res = new int[n];
+        int j = 0 ;
+        for(int i =1;i*i<=n;i++){
+            if(n%i == 0){
+              res[j++] = i;
+              if(n/i != n && i!=n/i){
+                  res[j++] = n/i;
+              }
+            }
+        }
+        return Arrays.copyOf(res,j);
+    }
+    public static void main(String[] args) {
+       int[] ans = mul(36);
+        System.out.println(Arrays.toString(ans));
+    }
+}
