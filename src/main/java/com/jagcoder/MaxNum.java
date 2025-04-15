2795721db6f3d97b3d9756bfde7053400b4df920
raghavendru <raghavendruppar@gmail.com>
2025-04-15 14:58:51 +0530

    max num is done

---
+++ src/main/java/com/jagcoder/MaxNum.java
@@ -0,0 +1,28 @@
+package com.jagcoder;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class MaxNum {
+    public int max(int n){
+        if (n==0) return 0;
+        int min = Integer.MIN_VALUE;
+        while(n>0){
+            int res = n%10;
+            if(res>min){
+                min = res;
+            }
+            n = n/10;
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER A NUMBER");
+        int n = sc.nextInt();
+        MaxNum m = new MaxNum();
+        int res = m.max(n);
+        System.out.println(res);
+    }
+}
