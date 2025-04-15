bd10e7d737b631cf698a75e48c7307d85d6195ac
raghavendru <raghavendruppar@gmail.com>
2025-04-15 18:30:40 +0530

    lcm done

---
+++ src/main/java/com/jagcoder/Lcm.java
@@ -0,0 +1,37 @@
+package com.jagcoder;
+
+public class Lcm {
+//    public static int gcd(int n1,int n2){
+//        while (n1>0 && n2>0){
+//            if(n1>n2){
+//                n1 = n1%n2;
+//            }
+//            else {
+//                n2 = n2%n1;
+//            }
+//        }
+//        if(n1 == 0) return n2;
+//        return n1;
+//
+//    }
+
+    public static int lcm(int n1,int n2){
+       int max = Math.max(n1,n2);
+       int mul = 1;
+       int lcm ;
+       int i = 1;
+      while (true){
+          mul = max*i;
+          if(mul%n1 ==0 && mul%n2 ==0){
+              lcm = mul;
+              break;
+          }
+          i++;
+      }
+      return lcm;
+    }
+    public static void main(String[] args) {
+      int res = lcm(4,6);
+        System.out.println(res);
+    }
+}
