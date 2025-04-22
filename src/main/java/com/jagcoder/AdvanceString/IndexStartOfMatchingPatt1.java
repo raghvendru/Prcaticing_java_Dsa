7aa0819454f57d1dba97c3de542609c70d70d0bd
raghavendru <raghavendruppar@gmail.com>
2025-04-22 18:24:58 +0530

    IndexStartOfMatchingPatt updated and impletemented in new way done

--- src/main/java/com/jagcoder/AdvanceString/IndexStartOfMatchingPatt1.java
+++ src/main/java/com/jagcoder/AdvanceString/IndexStartOfMatchingPatt1.java
@@ -2,34 +2,43 @@ package com.jagcoder.AdvanceString;
 import java.util.*;
 
 public class IndexStartOfMatchingPatt1 {
-    public static List<Integer> search(String text, String pat) {
-        int m = text.length(), n = pat.length(), p = 7, mod = 101;
-        int power = 1;
-        for (int i = 0; i < n - 1; i++)
-            power = (power * p) % mod;
+   public static List<Integer> search(String pat,String text){
+       int n = pat.length();
+       int m= text.length();
+       int pRight =1;
+       int pLeft = 1;
+       int p = 7;
+       int mod = 101;
+       int hashPat = 0;
+       int hashText = 0;
+       List<Integer> ans = new ArrayList<>();
+       for(int i=0;i<n;i++){
+           hashPat = (hashPat + ((pat.charAt(i)-'a'+1)*pRight)%mod)%mod;
+           hashText = (hashText+((text.charAt(i)-'a'+1)*pRight)%mod)%mod;
+           pRight = (pRight*p)%mod;
+       }
 
-        int hashPat = 0, hashTxt = 0;
-        for (int i = 0; i < n; i++) {
-            hashPat = (hashPat * p + (pat.charAt(i) - 'a' + 1)) % mod;
-            hashTxt = (hashTxt * p + (text.charAt(i) - 'a' + 1)) % mod;
-        }
+       for(int i = 0;i<=m-n;i++){
+           if(hashPat==hashText){
+              // if(text.substring(i,i+n).equals(pat)){
+                   ans.add(i);
 
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i <= m - n; i++) {
-            if (hashTxt == hashPat && text.substring(i, i + n).equals(pat)) {
-                ans.add(i);
-            }
-            if (i < m - n) {
-                hashTxt = (hashTxt - (text.charAt(i) - 'a' + 1) * power % mod + mod) % mod;
-                hashTxt = (hashTxt * p + (text.charAt(i + n) - 'a' + 1)) % mod;
-            }
-        }
-        return ans;
-    }
+               //}
+           }
+           if(i<m-n){
+               hashText = (hashText - ((text.charAt(i)-'a'+1)*pLeft)%mod+mod)%mod;
+               hashText = (hashText + ((text.charAt(i+n)-'a'+1)*pRight)%mod)%mod;
+               hashPat = (hashPat*p)%mod;
+               pLeft = (pLeft*p)%mod;
+               pRight =(pRight*p)%mod;
+           }
+       }
+       return ans;
+   }
 
     public static void main(String[] args) {
         String text = "gabcdeabc";
         String pat  = "abc";
-        System.out.println(search(text, pat));  // prints [1, 6]
+        System.out.println(search( pat,text));  // prints [1, 6]
     }
 }
